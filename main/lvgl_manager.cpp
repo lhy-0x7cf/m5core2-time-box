59ead8b2157234fce485c22a1a5287686f454eb3
cyclexit <cyclexit@outlook.com>
2022-01-01 21:41:36 -0500

    Use a thread to get and display the battery percentage to reduce CPU usage

--- main/lvgl_manager.cpp
+++ main/lvgl_manager.cpp
@@ -1,4 +1,6 @@
 // std library
+#include <chrono>
+#include <thread>
 
 // other libraries
 #include <lvgl.h>
@@ -24,11 +26,16 @@ SemaphoreHandle_t xGuiSemaphore;
 static const uint32_t kLvTickPeriodMs = 1;
 
 void drawBatteryPercentage() {
-  uint32_t battery_percentage = PowerManager::instance().getBatteryLevel();
   // battery percentage text
+  static uint32_t battery_percentage;
   static lv_obj_t *battery_percentage_text = lv_label_create(lv_scr_act());
   lv_obj_align(battery_percentage_text, LV_ALIGN_TOP_RIGHT, -5, 5);
-  lv_label_set_text_fmt(battery_percentage_text, "%d", battery_percentage);
+  while (true) {
+    battery_percentage = PowerManager::instance().getBatteryLevel();
+    printf("[%s] battery_percentage=%d\n", __FUNCTION__, battery_percentage);
+    lv_label_set_text_fmt(battery_percentage_text, "%d%%", battery_percentage);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
 }
 
 static void lv_tick_task(void *args) {
@@ -51,12 +58,12 @@ void guiTask(void *pvParameter) {
   ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, kLvTickPeriodMs * 1000));
 
   // gui code
-  // drawBatteryPercentage();
+  std::thread battery_status_thread(drawBatteryPercentage);
   
   // forever loop
   while (1) {
     /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
-    drawBatteryPercentage();
+    // drawBatteryPercentage();
     vTaskDelay(pdMS_TO_TICKS(10));
 
     /* Try to take the semaphore, call lvgl related function on success */
@@ -64,6 +71,11 @@ void guiTask(void *pvParameter) {
       lv_task_handler();
       xSemaphoreGive(xGuiSemaphore);
     }
+  }
+
+  // join the thread
+  if (battery_status_thread.joinable()) {
+    battery_status_thread.join();
   }
 }
 
--- main/power_manager.cpp
+++ main/power_manager.cpp
@@ -36,7 +36,7 @@ uint32_t PowerManager::getBatteryLevel() {
     // Converting method copied from https://github.com/m5stack/M5Core2/blob/master/src/AXP192.cpp#L269
     printf("battery_voltage=%0.2f\n", battery_voltage);
     float battery_percentage = (battery_voltage < 3.248088) ? 0 : (battery_voltage - 3.120712) * 100;
-    printf("battery_percentage=%0.2f\n", battery_percentage);
+    // printf("battery_percentage=%0.2f\n", battery_percentage);
     return static_cast<uint32_t>(std::min(battery_percentage, 100.0f));
   }
   printf("Failed to read the battery voltage.");
