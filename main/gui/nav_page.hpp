558ae4eac96614f5ba6c95f84ad45890834e81db
cyclexit <cyclexit@outlook.com>
2022-01-07 10:45:45 -0500

    Add comments to drawNavPage() function

--- main/gui/nav_page.hpp
+++ main/gui/nav_page.hpp
@@ -1,6 +1,10 @@
 #ifndef M5CORE2_TIME_BOX_GUI_NAV_PAGE_HPP_
 #define M5CORE2_TIME_BOX_GUI_NAV_PAGE_HPP_
 
+/**
+ * @brief If the nav page is drawn once, this function will not draw it again.
+ *        Instead, it will just show the GUI objects by removing LV_OBJ_FLAG_HIDDEN.
+ */
 void drawNavPage();
 
 void hideNavPage();
