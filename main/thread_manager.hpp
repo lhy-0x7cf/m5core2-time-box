76d76f4f3f7abc21d7c15c160d574a76f362db37
cyclexit <cyclexit@outlook.com>
2022-01-08 18:13:59 -0500

    Add some notes to ThreadManager class

--- main/thread_manager.hpp
+++ main/thread_manager.hpp
@@ -9,6 +9,20 @@
 
 // project header files
 
+/**
+ * NOTE:
+ * This thread manager is not really a manager, since I cannot find a way to
+ * join threads when the physical shutdown happens. It is more like a simple
+ * and naive thread pool to hold threads in one place.
+ * 
+ * I try the method here: http://cplusplus.com/forum/general/190480/#msg921110.
+ * I add an atomic to indicate whether the system is running or not and pass it
+ * into some function with an infinite loop like drawBatteryPercentage() to
+ * control the lifecycle of the loop. However, the log print after the loop
+ * cannot be found after I pressesd the reset button or power button. Besides,
+ * this method also bumps up the CPU usage a little bit, so I just choose not
+ * to use it.
+ */
 class ThreadManager {
  public:
   template<typename Func, typename... Args>
@@ -30,13 +44,7 @@ class ThreadManager {
   std::vector<std::thread> threads;
 
   ThreadManager() = default;
-  ~ThreadManager() {
-    for (int i = 0; i < threads.size(); ++i) {
-      if (threads[i].joinable()) {
-        threads[i].join();
-      }
-    }
-  }
+  ~ThreadManager() = default;
 };
 
 #endif
