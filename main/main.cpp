29b696651e655d57511cadadeb68084e3e89b6fd
cyclexit <cyclexit@outlook.com>
2022-01-11 15:48:37 -0500

    Get wifi init working

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,28 @@
 // std library
 
 // other libraries
+#include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <nvs_flash.h>
 
 // project header files
 #include "gui/lvgl_manager.hpp"
 #include "power_manager.hpp"
 #include "wifi_manager.hpp"
 
+void init_nvs() {
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+}
+
 extern "C" void app_main(void) {
+  init_nvs();
   PowerManager::instance().setup();
   LvglManager::instance().start();
-  // WifiManager::instance().init_sta();
+  WifiManager::instance().init_sta();
 }
--- main/wifi_manager.cpp
+++ main/wifi_manager.cpp
@@ -13,6 +13,8 @@ void WifiManager::init_sta() {
   // TODO: fully implement this
   ESP_ERROR_CHECK(esp_netif_init());
   esp_netif_create_default_wifi_sta();
-  // wifi_init_config_t wifi_init_cfg = WIFI_INIT_CONFIG_DEFAULT();
-  // ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_cfg));
+  wifi_init_config_t wifi_init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+  printf("init_config size=%d\n", sizeof(wifi_init_config_t));
+  esp_err_t err = esp_wifi_init(&wifi_init_cfg);
+  printf("%s\n", esp_err_to_name(err));
 }
