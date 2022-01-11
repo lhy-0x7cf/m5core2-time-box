a678e31cb6012fb57374d8fdc0605ab0ad5bc25a
cyclexit <cyclexit@outlook.com>
2022-01-10 23:01:22 -0500

    Add the docs about how to avoid mis-click when swiping

--- main/gui/utils.hpp
+++ main/gui/utils.hpp
@@ -8,6 +8,13 @@
 
 // project header files
 
+/**
+ * NOTE:
+ * Calling enablePressLock(lv_obj_t *obj) on background objects (e.g., lv_scr_act())
+ * and calling disablePressLock(lv_obj_t *obj) on foreground objects. This will make
+ * sure that the gesture event for background objects will not cause the click event
+ * for foreground objects
+ */
 void enablePressLock(lv_obj_t *obj);
 
 void disablePressLock(lv_obj_t *obj);
